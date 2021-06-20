21f6e041963df9e41b27797b734e28c70bc48c25
Samurus <achenbachj@web.de>
2021-06-20 23:23:18 +0200

    CascadeType.DETACH

--- src/main/java/Riddle.java
+++ src/main/java/Riddle.java
@@ -30,7 +30,7 @@ public class Riddle {
 
     private String question;
 
-    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
     private List<KeyWord> children;
 
 }
